243ba033150fb2b8b9d98bfa9ed751062c9576e3
Matheus Icaro <matheusicaro2@hotmail.com>
2018-08-26 22:14:46 -0300

    implementing expression "flow" for calculation.

--- scripts/controllers/CalculatorController.js
+++ scripts/controllers/CalculatorController.js
@@ -6,7 +6,7 @@ class CalculatorController {
         this._displayValueResult = document.querySelector('#display');
         this._displayHour = document.querySelector('#hora');
         this._displayDate = document.querySelector('#data');
-        this._ExpressionCalculate = [];
+        this._ExpressionCalculate = [];             //expressao (vetor)
         this.initialize();
         this.initButtonsEvents();
     }
@@ -51,23 +51,31 @@ class CalculatorController {
 
         switch (buttonValue) {
             case "ac":
+                this.clearDisplayExpression();
                 break;
             case "ce":
 
                 break;
             case "porcento":
+                this.addOperation("%");
                 break;
             case "divisao":
+                this.addOperation("/");    
                 break;
             case "multiplicacao":
+                this.addOperation("*");
                 break;
             case "subtracao":
+                this.addOperation("-");
                 break;
             case "soma":
+                this.addOperation("+");
                 break;
             case "igual":
+                this.addOperation("=");
                 break;
             case "ponto":
+                this.addOperation(".");
                 break;
             case "0":        
             case "1":
@@ -80,6 +88,7 @@ class CalculatorController {
             case "8":
             case "9":
             case "10":
+                this.addOperation(parseInt(buttonValue));
                 break;
             default:
                 this.setDisplayValueResult = "Error";
@@ -91,6 +100,99 @@ class CalculatorController {
         this.setDisplayValueResult = "0";
         this.setExpressionCalculate = [];
     };
+
+    printDisplay(){
+        console.log("imprimindo..:", this.getExpressionCalculate);
+    }
+
+    addOperation(currentValue){
+
+        // if the last value of expression is different of number, AND, not exist expression, enter.
+        if(isNaN(this.lastExpressionValue())){     
+
+            if(this.isOperator(currentValue) && this.isOperator(this.lastExpressionValue())){
+                this._ExpressionCalculate[this.replaceLastExpression()] = value;
+            
+            }else if( this.isDot(currentValue) || this.isDot(this.lastExpressionValue()) ){
+    
+                if(this.isOperator(this.lastExpressionValue())){
+                    this.pushExpressionCalculate(currentValue)
+                }else{
+                    let newValue = this.lastExpressionValue().toString() + currentValue.toString();
+                    let replaceLastExpression = true;
+                    this.pushExpressionCalculate(parseFloat(newValue), replaceLastExpression);
+                }
+            }else
+                this.pushExpressionCalculate(currentValue)
+        }else{
+
+            if(this.isOperator(currentValue)){
+                this.pushExpressionCalculate(currentValue);
+
+            }else if( this.isDot(currentValue) && this.expressionCalculateIsEmpty()){
+                this.pushExpressionCalculate(currentValue);
+                
+            }else{
+                let newValue = this.lastExpressionValue().toString() + currentValue.toString();
+                let replaceLastExpression = true;
+                this.pushExpressionCalculate(parseFloat(newValue), replaceLastExpression);
+            }
+        }
+
+        this.printDisplay();
+    }
+
+    pushExpressionCalculate(value, conditionReplace){
+
+        if(this.expressionCalculateIsEmpty()){
+            this.setExpressionCalculate = value;
+            return
+        }
+        if(conditionReplace){
+            this._ExpressionCalculate[this.lastIndexOfExpression()] = value;
+            return
+        }else if( this._ExpressionCalculate.length < 3){
+            this.setExpressionCalculate = value;
+            return
+        }
+        console.log("pronto para somar")
+
+    }
+
+    expressionCalculateIsEmpty(){
+        if(this.getExpressionCalculate.length === 0)
+            return true;
+        else
+            return false;
+    }
+
+    lastIndexOfExpression(){
+        return (this._ExpressionCalculate.length - 1);
+    }
+
+    lastExpressionValue(){
+        if(this.expressionCalculateIsEmpty()){
+            return this.getExpressionCalculate;
+        }
+        return this._ExpressionCalculate[this.lastIndexOfExpression()];
+    }
+
+    replaceLastExpression(){
+        if(this.expressionCalculateIsEmpty())
+            return this._ExpressionCalculate.length
+        return this.lastIndexOfExpression();
+    }
+
+    isOperator(value){
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
+    }
+
+    isDot(value){
+        if(value == ".")
+            return true
+        return false
+    }
+
 
     get getExpressionCalculate(){
         return this._ExpressionCalculate;
